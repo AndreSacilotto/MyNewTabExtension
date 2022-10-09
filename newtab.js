984fdc7787bf70abe0ae45e4d88b2a65327ed1f3
Andre Sacilotto <66838864+AndreSacilotto@users.noreply.github.com>
2022-10-08 23:49:59 -0300

    Use the new favicon API instead

--- newtab.js
+++ newtab.js
@@ -1,13 +1,13 @@
 
-class Item {
+class Item
+{
 	/** @param {string} name @param {string} url */
-	constructor(name, url) {
+	constructor(name, url)
+	{
 		this.name = name;
 		this.url = url;
 	}
 }
-
-const favicon2 = "https://www.google.com/s2/favicons?sz=128&domain_url="
 
 const list = [
 	new Item("Google", "https://www.google.com/"),
@@ -27,23 +27,25 @@ const list = [
 	new Item("Source", "https://github.com/Eloston/ungoogled-chromium"),
 ]
 
-for (let i = 0; i < list.length; i++) {
+for (let i = 0; i < list.length; i++)
+{
 	let shortcut = generateShortcut(list[i]);
 	document.body.appendChild(shortcut);
 }
 
 /** @param {Item} item @returns {HTMLElement} */
-function generateShortcut(item) {
+function generateShortcut(item)
+{
 	const a = document.createElement("a");
 	a.href = item.url;
 
 	const divImg = document.createElement("div");
 	divImg.className = "div-icon";
-	
+
 	const img = document.createElement("img");
 	img.alt = item.name;
-	img.src = encodeURI(favicon2 + item.url);
-	
+	img.src = getFaviconUrl2(item.url);
+
 	const divText = document.createElement("div");
 	divText.className = "div-text";
 	divText.innerHTML = item.name;
@@ -59,6 +61,29 @@ var fixed = document.getElementById("fixed-black");
 document.getElementById("button-black").onclick = switchBlack;
 fixed.onclick = switchBlack;
 
-function switchBlack() {
+function switchBlack()
+{
 	fixed.hidden = !fixed.hidden;
+}
+
+/* OLD - Manifest v3.0 workround 
+function getFaviconUrl(url)
+{
+	if(!url || url.length === 0)
+		return "";
+	const favicon2 = "https://www.google.com/s2/favicons?sz=128&domain_url="
+	return encodeURI(favicon2 + url);
+}
+*/
+
+/** @param {String} url */
+function getFaviconUrl2(url)
+{	
+	if(!url || url.length === 0)
+		return "";
+	// return `chrome-extension://${chrome.runtime.id}/_favicon/?pageUrl=${encodeURIComponent(url)}&size=32`;
+	let faviconUrl = new URL(`chrome-extension://${chrome.runtime.id}/_favicon/`);
+	faviconUrl.searchParams.append('pageUrl', url);
+	faviconUrl.searchParams.append('size', '32');
+	return faviconUrl.href;
 }
