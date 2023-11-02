7741b97d257b64e2326bfba9be25a6bee3b40fa2
Andre Sacilotto <66838864+AndreSacilotto@users.noreply.github.com>
2023-11-02 16:20:26 -0300

    support FHD

--- newtab.js
+++ newtab.js
@@ -9,29 +9,34 @@ class Item
 	}
 }
 
+// DATA
+
 const list = [
-	new Item("Google", "https://www.google.com/"),
 	new Item("Youtube", "https://www.youtube.com/"),
 	new Item("Translate", "https://translate.google.com.br/?sl=pt&tl=en&op=translate"),
+	new Item("", ""),
+	new Item("Regex", "https://regex101.com/"),
 	new Item("Desmos", "https://www.desmos.com/calculator"),
-	new Item("Symbolab", "https://www.symbolab.com/"),
-	new Item("Sauce", "https://saucenao.com/"),
-	new Item("My Git", "https://github.com/AndreSacilotto?tab=repositories"),
-	new Item("ZapZap", "https://web.whatsapp.com/"),
+	
 	new Item("Drive", "https://drive.google.com/drive/my-drive"),
 	new Item("", ""),
 	new Item("", ""),
+	new Item("R3", "https://www.4devs.com.br/calculadora_regra_tres_simples"),
+	new Item("Symbolab", "https://www.symbolab.com/"),
+	
+	new Item("Sauce", "https://saucenao.com/"),
 	new Item("", ""),
-	new Item("", ""),
-	new Item("Extensions", "https://chrome.google.com/webstore/category/extensions"),
+	new Item("ZapZap", "https://web.whatsapp.com/"),
+	new Item("My Git", "https://github.com/AndreSacilotto?tab=repositories"),
 	new Item("Source", "https://github.com/Eloston/ungoogled-chromium"),
 ]
 
-for (let i = 0; i < list.length; i++)
-{
-	let shortcut = generateShortcut(list[i]);
-	document.body.appendChild(shortcut);
-}
+const imgSize = 32;
+
+// FUNCTIONS
+const container = document.getElementById("container");
+
+list.forEach(item => container.appendChild(generateShortcut(item)));
 
 /** @param {Item} item @returns {HTMLElement} */
 function generateShortcut(item)
@@ -42,15 +47,18 @@ function generateShortcut(item)
 	const divImg = document.createElement("div");
 	divImg.className = "div-icon";
 
-	const img = document.createElement("img");
-	img.alt = item.name;
-	img.src = getFaviconUrl2(item.url);
-
 	const divText = document.createElement("div");
 	divText.className = "div-text";
 	divText.innerHTML = item.name;
+	
+	if(item.url){
+		const img = document.createElement("img");
+		img.alt = item.name;
+		img.src = getFaviconUrl(item.url);
+		img.width = imgSize;
+		divImg.appendChild(img);
+	}
 
-	divImg.appendChild(img);
 	a.appendChild(divImg);
 	a.appendChild(divText);
 
@@ -66,24 +74,20 @@ function switchBlack()
 	fixed.hidden = !fixed.hidden;
 }
 
-/* OLD - Manifest v3.0 workround 
+/** @param {String} url @returns {String} */
 function getFaviconUrl(url)
-{
-	if(!url || url.length === 0)
-		return "";
-	const favicon2 = "https://www.google.com/s2/favicons?sz=128&domain_url="
-	return encodeURI(favicon2 + url);
-}
-*/
-
-/** @param {String} url */
-function getFaviconUrl2(url)
 {	
 	if(!url || url.length === 0)
 		return "";
-	// return `chrome-extension://${chrome.runtime.id}/_favicon/?pageUrl=${encodeURIComponent(url)}&size=32`;
-	let faviconUrl = new URL(`chrome-extension://${chrome.runtime.id}/_favicon/`);
+
+	let faviconUrl;
+
+	if(chrome.runtime)
+		faviconUrl = new URL(`chrome-extension://${chrome.runtime.id}/_favicon/`);
+	else
+		faviconUrl = new URL(`https://www.google.com/s2/favicons?sz=128&domain_url=${url}`);
+
 	faviconUrl.searchParams.append('pageUrl', url);
-	faviconUrl.searchParams.append('size', '32');
+	faviconUrl.searchParams.append('size', imgSize.toString());
 	return faviconUrl.href;
 }
